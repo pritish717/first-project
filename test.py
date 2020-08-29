0c44fbea55bb80a053a3eb764a0b890692965c24
Pritish Chaudhary <pritish717@gmail.com>
2020-08-29 11:55:10 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import os
+
+print('EMAIL_USER', os.environ.get('EMAIL_USER'))
+print('EMAIL_PASS', os.environ.get('EMAIL_PASS'))
+print('SECRET_KEY', os.environ.get('SECRET_KEY'))
+print('DEBUG_VALUE', os.environ.get('DEBUG_VALUE'))
+print('AWS_ACCESS_KEY_ID', os.environ.get('AWS_ACCESS_KEY_ID'))
+print('AWS_SECRET_ACCESS_KEY', os.environ.get('AWS_SECRET_ACCESS_KEY'))
+print('AWS_STORAGE_BUCKET_NAME', os.environ.get('AWS_STORAGE_BUCKET_NAME'))
+print('AWS_S3_REGION_NAME', os.environ.get('AWS_S3_REGION_NAME'))
+# print('EMAIL_USER', os.environ.get('EMAIL_USER'))
+
